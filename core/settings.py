724be365f72f77505a63a4206961569d8a820901
Jayendra Khandare <jayendrakhandare@gmail.com>
2021-05-09 22:31:19 +0530

    added authentication and user_management apps

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'user_management',
 ]
 
 MIDDLEWARE = [
