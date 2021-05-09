2f03340dbcc654f521102b8ca33d75b6058607a9
Jayendra Khandare <jayendrakhandare@gmail.com>
2021-05-10 00:40:06 +0530

    refactored code for understanding

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from authentication.forms import LoginForm, SignUpForm
@@ -10,11 +8,9 @@ from random import randint
 
 def login_view(request):
     form = LoginForm(request.POST or None)
-
     msg = None
 
     if request.method == "POST":
-
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
@@ -33,7 +29,6 @@ def login_view(request):
     return render(request, "accounts/login.html", {"form": form, "msg" : msg})
 
 def register_user(request):
-
     msg     = None
     success = False
 
@@ -43,7 +38,6 @@ def register_user(request):
             form.save()
             first_name = form.cleaned_data.get("first_name")
             last_name = form.cleaned_data.get("last_name")
-            username = form.cleaned_data.get("first_name").lower()[0] + form.cleaned_data.get("last_name").lower()
 
             try:
                 usr_obj = User.objects.get(first_name=first_name, last_name=last_name, username="")
@@ -51,17 +45,19 @@ def register_user(request):
                 print(e)
             else:
                 if DEBUG:
+                    # decide on username as auser for Axel User
+                    username = form.cleaned_data.get("first_name").lower()[0] + form.cleaned_data.get("last_name").lower()
                     usr_obj.username = username
                     usr_obj.save()
                     user = authenticate(username=username, password=form.cleaned_data.get("password1"))
                     login(request, user)
-                    return redirect("/login/")
+                    return redirect("login")
                 else: 
                     # this means we have to setup a temporary username
                     usr_obj.username = str(randint(1,1000))
                     usr_obj.save()
 
-            msg     = 'User created - please provide documentation for verification'
+            msg     = 'User created {}- please provide documentation for verification'.format(usr_obj.username)
             success = True
 
         else:
@@ -70,3 +66,4 @@ def register_user(request):
         form = SignUpForm()
 
     return render(request, "accounts/register.html", {"form": form, "msg" : msg, "success" : success })
+
